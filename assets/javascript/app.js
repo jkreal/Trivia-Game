645420dab47b6191b33d0e5d525d4b1bed8cfeea
jkrowden <jkrowden@gmail.com>
2018-02-21 13:24:52 -0700

    Made the layout and testing a simple timer

---
+++ assets/javascript/app.js
@@ -0,0 +1,75 @@
+var questionArray = [];
+var question = {
+
+	question: {
+
+		question: '',
+
+		rightAnswer: '',
+		wrongAnswer: '',
+
+		rightAnswerID: '',
+
+		answer1: {
+			answer: '',
+			correct: ''
+		},
+
+		answer2: {
+			answer: '',
+			correct: ''
+		},
+
+		answer3: {
+			answer: '',
+			correct: ''
+		},
+
+		answer4: {
+			answer: '',
+			correct: ''
+		}
+
+	}
+
+}
+
+window.onload = function () {
+	var intervalId;
+	timer.run();
+	console.log("loaded");
+
+	$.ajax({
+		type: "GET",
+		url: "questions.html"
+	}).then(function(response) {
+		console.log(response.question1.question);
+	});
+}
+
+
+var timer = {
+	number: 30,
+
+	run: function () {
+		number = 30;
+		intervalId = setInterval(function () {
+			this.number--;
+			console.log(this.number);
+
+			$('#timer').text(this.number);
+			console.log("tick");
+			if (this.number == 0) {
+				this.number = 0;
+				clearInterval(this.intervalId);
+				stop();
+			}
+
+		}, 100);
+	},
+
+	stop: function () {
+		clearInterval(IntervalId);
+	}
+
+}
