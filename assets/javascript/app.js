4a766d7f3d797a63ad29f5c03cb0e2331b03010d
jkrowden <jkrowden@gmail.com>
2018-02-22 17:03:38 -0700

    Added more functionality through javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,69 +1,120 @@
-var questionArray = [];
-var question = {
-
-	question: {
-
-		question: '',
-
-		rightAnswer: '',
-		wrongAnswer: '',
-
-		rightAnswerID: '',
-
-		answer1: {
-			answer: '',
-			correct: ''
-		},
-
-		answer2: {
-			answer: '',
-			correct: ''
-		},
-
-		answer3: {
-			answer: '',
-			correct: ''
-		},
-
-		answer4: {
-			answer: '',
-			correct: ''
-		}
-
-	}
-
-}
+var currentQuestion = questions[0];
+var prevQuestions = [];
+var questionNumber = 1;
+var gameRunning = true;
+var intervalId;
+var number;
 
 window.onload = function () {
-	var intervalId;
 	timer.run();
-	console.log("loaded");
+	randomQuestion();
+	updateContent();
+}
+
+function randomQuestion() {
+var questionNum = 0;
+
+	do {
+		questionNum++;
+		currentQuestion = questions[Math.floor(Math.random() * (questions.length))];
+
+		if(questionNum >= questions.length){
+			break;
+		}
+	} while (prevQuestions.includes(currentQuestion));
 
 }
 
+function storeQuestion() {
+	prevQuestions.push(currentQuestion);
+}
 
 var timer = {
-	number: 30,
 
 	run: function () {
-		number = 30;
+		number = 20;
 		intervalId = setInterval(function () {
 			this.number--;
-			console.log(this.number);
-
 			$('#timer').text(this.number);
-			console.log("tick");
+
+
 			if (this.number == 0) {
-				this.number = 0;
-				clearInterval(this.intervalId);
+				clearInterval(intervalId);
+				gameOver();
 				stop();
 			}
 
-		}, 100);
+		}, 1000);
 	},
 
 	stop: function () {
-		clearInterval(IntervalId);
+		clearInterval(intervalId);
 	}
 
+}
+
+function updateContent() {
+	$('#title').text("Question #" + questionNumber);
+	$('#question').text(currentQuestion.question);
+
+	$('.answers').children().children().each(function (index, current) {
+		$(current).text(currentQuestion.answers[index]);
+	});
+
+}
+
+$('.answer-button').on("click", function () {
+	console.log($(this).text());
+	if (gameRunning) {
+		if ($(this).text() === currentQuestion.correct) {
+			console.log("success");
+			showAnswer(true);
+		}
+		else {
+			console.log("wrong answer");
+			showAnswer(false);
+		}
+	}
+});
+
+function showAnswer(correct) {
+	timer.stop();
+	$('#next-button').show();
+	if (correct) {
+		$('#title').text("Correct!");
+	}
+	else {
+		$('#title').text("Incorrect!");
+	}
+
+	$('.answers').children().children().each(function (index, current) {
+		if ($(this).text() !== currentQuestion.correct) {
+			$(this).hide();
+		}
+	});
+}
+
+function resetButtons() {
+	$('.answers').children().children().each(function (index, current) {
+		$(current).show();
+	});
+}
+
+$('#next-button').on("click", function () {
+	if(questionNumber < questions.length){
+		resetButtons();
+		timer.run();
+		$(this).hide();
+		questionNumber++;
+		$('#title').text("Question #" + questionNumber);
+		randomQuestion();
+		updateContent();
+	}
+	else {
+		console.log("over");
+	}
+});
+
+function gameOver(){
+	$('#timer').text("Time's Up!");
 }
---
+++ assets/javascript/questions.js
@@ -0,0 +1,22 @@
+var questions = [
+
+	{
+		question: "What kind of bear is best?", answers: ["Black Bear", "Grizzly Bear", "Polar Bear", "Other Bear"], correct: "Black Bear", imgsrc: "../images/captain-placeholder.jpg"
+	},
+
+	{
+		question: "Ulysses S. Grant appears on the front of which denomination of US. currency?", answers: ["$10 Bill", "$20 Bill", "$50 Bill", "$100 Bill"], correct: "$50 Bill", imgsrc: "../images/captain-placeholder.jpg"
+	},
+
+	{
+		question: "Which U.S. president appears on the front of the $2 bill?", answers: ["Andrew Jackson", "Grizzly Bear", "Thomas Jefferson", "George Washington"], correct: "Thomas Jefferson", imgsrc: "../images/captain-placeholder.jpg"
+	},
+
+	{
+		question: "Which U.S. state has the longest coastline?", answers: ["California", "Florida", "Texas", "Alaska"], correct: "Alaska", imgsrc: "../images/captain-placeholder.jpg"
+	},
+
+	{
+		question: "What is the capitol of Hawaii?", answers: ["Kailua", "Makaha", "Pearl City", "Honolulu"], correct: "Honolulu", imgsrc: "../images/captain-placeholder.jpg"
+	}
+];
